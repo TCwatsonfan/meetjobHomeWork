25f94665b5aaacda95ae855be35c753fb21605d6
TCwatsonfan <109008281+TCwatsonfan@users.noreply.github.com>
2022-08-02 19:35:41 +0800

    測試用

---
+++ Ex01.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  7 18:56:22 2022
+
+@author: watson
+"""
+# 單行註解
+#英文的大小寫有差，小s 大S  有差嗎？
+#變數的命名，會取有意思的代號
+name = "John"   #型態：字串 文字
+age = 20    #整數
+rate = 0.18   #浮點數
+check = True   #布林值
+
+print(name)     # 印出 name 的變數內容
+print(type(name))  # type 是顯示型態 str-> string(字串)
+print(type(age))   #  int -> integer (整數) 
+print(type(rate))   # float  ->浮點數
+print(type(check))  # bool -->值只有二個：True, False
+
+name = 20
+print(type(name))
+print(name)
+
+
